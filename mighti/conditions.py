7085c16758e7c5c7a616c4e80c94cf3893e186c1
Nao Yamamoto <nao.m.yamamoto@gmail.com>
2025-03-13 13:48:18 -0400

    interactions working!!!

--- mighti/conditions.py
+++ mighti/conditions.py
@@ -12,6 +12,86 @@ import pandas as pd
 ncds = []  # Ensures ncds exists even if initialize_conditions() fails
 communicable_diseases = []
 
+
+# # Define Type2Diabetes condition
+# class Type2Diabetes(ss.NCD):
+
+#     def __init__(self, pars=None, **kwargs):
+#         super().__init__()
+#         self.define_pars(
+#             dur_condition=ss.lognorm_ex(15.028971), 
+#             incidence_prob=0.01,
+#             incidence=ss.bernoulli(0.01),  
+#             p_death=ss.bernoulli(0.004315),  
+#             init_prev=ss.bernoulli(0.2), 
+#             remission_rate=ss.bernoulli(0.0024), 
+#             max_disease_duration=20, 
+#         )
+#         self.update_pars(pars, **kwargs)
+
+#         self.define_states(
+#             ss.State('susceptible', default=True),
+#             ss.State('affected'),
+#             ss.State('reversed'),  # New state for diabetes remission
+#             ss.FloatArr('ti_affected'),
+#             ss.FloatArr('ti_reversed'),
+#             ss.FloatArr('ti_dead'),
+#             ss.FloatArr('rel_sus', default=1.0),
+#         )
+#         return
+
+#     def init_post(self):
+#         initial_cases = self.pars.init_prev.filter()
+#         self.set_prognoses(initial_cases)
+#         return initial_cases
+
+#     def step_state(self):
+#         # Handle remission (reversal)
+#         going_into_remission = self.pars.remission_rate.filter(self.affected.uids)
+#         self.affected[going_into_remission] = False
+#         self.reversed[going_into_remission] = True
+#         self.ti_reversed[going_into_remission] = self.ti
+
+#         # Handle recovery, death, and beta-cell function exhaustion
+#         recovered = (self.reversed & (self.ti_reversed <= self.ti)).uids
+#         self.reversed[recovered] = False
+#         self.susceptible[recovered] = True  # Recovered individuals become susceptible again
+#         deaths = (self.ti_dead == self.ti).uids
+#         self.sim.people.request_death(deaths)
+#         self.results.new_deaths[self.ti] = len(deaths)
+
+#     def step(self):
+#         new_cases = self.pars.incidence.filter(self.susceptible.uids)
+#         self.set_prognoses(new_cases)
+#         return new_cases
+    
+#     def set_prognoses(self, uids):
+#         sim = self.sim
+#         p = self.pars
+#         self.susceptible[uids] = False
+#         self.affected[uids] = True
+#         dur_condition = p.dur_condition.rvs(uids)
+#         will_die = p.p_death.rvs(uids)
+#         dead_uids = uids[will_die]
+#         rec_uids = uids[~will_die]
+#         self.ti_dead[dead_uids] = self.ti + dur_condition[will_die] / self.t.dt
+#         self.ti_reversed[rec_uids] = self.ti + dur_condition[~will_die] / self.t.dt
+#         return
+
+#     def init_results(self):
+#         super().init_results()
+#         self.define_results(
+#             ss.Result('reversal_prevalence', dtype=float),
+#         )
+#         return
+
+#     def update_results(self):
+#         super().update_results()
+#         self.results.prevalence[self.ti] = np.count_nonzero(self.affected) / len(self.sim.people)
+#         self.results.reversal_prevalence[self.ti] = np.count_nonzero(self.reversed) / len(self.sim.people)
+#         return
+    
+    
 def initialize_conditions(df, ncd_list, sis_list):
     """ Load disease parameters and categories from `mighti_main.py`. """
     global df_params, ncds, communicable_diseases
