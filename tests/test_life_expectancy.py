379a4c5b1f3fa4fe370685da3b4acf2d6b0ebdf8
Nao Yamamoto <nao.m.yamamoto@gmail.com>
2025-06-12 12:13:07 -0400

    testing sys

--- tests/test_life_expectancy.py
+++ tests/test_life_expectancy.py
@@ -8,8 +8,6 @@ import pandas as pd
 import numpy as np
 import os
 import sys
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import mighti as mi
 
 from prepare_data_for_year import prepare_data_for_year
