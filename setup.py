4ce2eae7cf9d935dcde206b487f1d0982f9eae9b
Nao Yamamoto <64042180+madonnaojorin@users.noreply.github.com>
2025-08-14 19:13:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     install_requires=[
         'numpy',
         'starsim=2.3.2',
-        'stisim>=1.0.1',
+        'stisim=1.3.0',
         'scipy',
         'pandas>=2.0.0',
         'sciris>=3.0.0',
