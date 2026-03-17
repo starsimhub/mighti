205e8a2013fa2a9bffeb611ee0d7c7b8ffc1c60b
Nao Yamamoto <nao.m.yamamoto@gmail.com>
2026-03-17 15:23:23 -0400

    release: v0.1.0

--- mighti/util/version.py
+++ mighti/util/version.py
@@ -4,7 +4,7 @@ Version and license information.
 
 __all__ = ["__version__", "__versiondate__", "__license__"]
 
-__version__ = "0.0.3"
-__versiondate__ = "2026-03-03"
+__version__ = "0.1.0"
+__versiondate__ = "2026-03-17"
 __license__ = f"MIGHTI {__version__} ({__versiondate__}) — © 2024-2026 by New York University"
 
